1827deb9943dacc26f5f5e616f2087e2ff7b7000
a.maksimov <a.maksimov@ispsystem.com>
2018-05-03 10:24:10 +0800

    Removed logging

--- src/cmd.py
+++ src/cmd.py
@@ -5,9 +5,5 @@ import sys
 import os
 
 if __name__ == '__main__':
-    log = open('/tmp/cmd.log', 'a')
-    log.write('argv: ' + str(sys.argv) + '\n')
-    log.write('cwd: ' + os.getcwd() + '\n')
-    log.close()
     builder = RemoteBuilder(config)
     builder.execute()
