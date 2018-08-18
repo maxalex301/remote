53ee73bf744143f9007188ebb294f0266a7bbc5c
Aleksey S. Maksimov <maxalex301@gmail.com>
2018-08-18 23:42:19 +0800

    It workable

--- src/command.py
+++ src/command.py
@@ -8,13 +8,14 @@ CMAKE_CACHE_TXT = 'CMakeCache.txt'
 CMAKE_LISTS_TXT = 'CMakeLists.txt'
 CONAN_PY = 'conanfile.py'
 CONAN_TXT = 'conanfile.txt'
+CONAN_HOME = '.conan'
 
 class BuildEnv:
     def __init__(self, source_dir, build_dir):
         self.source_dir = source_dir
         self.build_dir = build_dir
-        self.project = os.path.basename(self.source_dir)
-        self.project_cbp = os.path.join(self.build_dir, self.project+'.cbp')
+        self.project = self.source_dir.name
+        self.project_cbp = self.build_dir / (self.project + '.cbp')
 
 
 class Command:
@@ -24,8 +25,6 @@ class Command:
         self.server = Server(self.config.BUILD_HOST,
                              self.config.BUILD_PORT,
                              self.config.BUILD_USER)
-        self.local = None
-        self.remote = None
 
     def is_version_check(self):
         return False
@@ -47,8 +46,6 @@ class Command:
         print("Uploading project...")
         self.upload_project()
 
-        # self.set_compiler_args()
-
         if self.run() != 0:
             return
 
@@ -56,24 +53,24 @@ class Command:
         self.download_artifacts()
 
     def run(self, command = ''):
-        return self.server.cmd_in_wd(self.remote.build_dir, command if command else ' '.join(escape(self.argv)))
+        return self.server.cmd_in_wd(str(self.remote.build_dir), command if command else ' '.join(escape(self.argv)))
 
     def create_remote_directories(self):
-        self.server.mkdir(self.remote.source_dir)
-        if os.path.exists(self.local.build_dir):
-            self.server.mkdir(self.remote.build_dir)
+        self.server.mkdir(str(self.remote.source_dir))
+        if self.local.build_dir.exists():
+            self.server.mkdir(str(self.remote.build_dir))
 
     def upload_project(self):
         self.create_remote_directories()
 
         excludes = self.config.EXCLUDES
-        if os.path.exists(self.local.build_dir):
-            excludes.append(os.path.basename(self.local.build_dir))
-        self.server.upload(self.local.source_dir, self.remote.source_dir, excludes)
+        if self.local.build_dir.exists():
+            excludes.append(self.local.build_dir.name)
+        self.server.upload(str(self.local.source_dir), str(self.remote.source_dir), excludes)
 
     def download_artifacts(self):
-        if os.path.exists(self.local.build_dir):
-            self.server.download(self.remote.build_dir, self.local.build_dir, ['.ssh'])
+        if self.local.build_dir.exists():
+            self.server.download(str(self.remote.build_dir), str(self.local.build_dir), ['.ssh'])
 
 
 class CMakeCommand(Command):
@@ -114,26 +111,27 @@ class CMakeCommand(Command):
 
     def replace_file_variables(self, file):
         self.server.replace_file_content(file,
-                                         '=' + self.remote.source_dir,
-                                         '=' + self.local.source_dir)
+                                         '=' + str(self.remote.source_dir),
+                                         '=' + str(self.local.source_dir))
 
 
     def make_configurations(self):
-        src_dir = self.argv[-1]
+        src_dir = Path(self.argv[-1]).resolve()
         if self.__is_build():
-            src_dir = os.path.dirname(os.path.abspath(self.argv[2]))
+            src_dir = Path(self.argv[2]).resolve().parent
             self.__need_upload = False
 
-        self.local = BuildEnv(os.path.abspath(src_dir), os.getcwd())
+        self.local = BuildEnv(src_dir, Path.cwd())
 
-        self.remote = BuildEnv(self.config.REMOTE_DIR + self.local.source_dir,
-                               self.config.REMOTE_DIR + self.local.build_dir)
+        remote = Path(self.config.REMOTE_DIR)
+        print("Remote: " + str(remote))
+        self.remote = BuildEnv(remote / str(self.local.source_dir)[1:], remote / str(self.local.build_dir)[1:])
 
         self.local.cmake_lists = Path(self.local.source_dir) / CMAKE_LISTS_TXT
         self.local.cmake_cache = Path(self.local.build_dir) / CMAKE_CACHE_TXT
         self.remote.cmake_cache = Path(self.remote.build_dir) / CMAKE_CACHE_TXT
 
-        print("remote: " + self.remote.source_dir + "   " + self.remote.build_dir)
+        print("remote: " + str(self.remote.source_dir) + "   " + str(self.remote.build_dir))
 
 
     def __run_build(self):
@@ -143,21 +141,21 @@ class CMakeCommand(Command):
 
 
     def run(self):
-        assert os.path.exists(self.local.cmake_lists), "CMakeLists.txt does not exists in source directory " + self.local.cmake_lists
+        assert self.local.cmake_lists.exists(), "CMakeLists.txt does not exists in source directory " + str(self.local.cmake_lists)
 
         if self.__is_build():
             self.__run_build()
             return
         else:
-            self.argv[-1] = self.remote.source_dir
+            self.argv[-1] = str(self.remote.source_dir)
             self.argv[0] = self.config.CMAKE
             super(CMakeCommand, self).run()
 
         if self.__is_toolset_check():
             self.replace_file_variables(self.remote.cmake_cache)
             self.modify_cmake_cache()
-            self.server.rm(self.remote.build_dir)
-            self.server.rm(self.remote.source_dir)
+            self.server.rm(str(self.remote.build_dir))
+            self.server.rm(str(self.remote.source_dir))
 
     def __is_ninja_build(self):
         return self.config.NINJA is not None and self.config.NINJA
@@ -165,7 +163,7 @@ class CMakeCommand(Command):
     def __generate(self):
         command = [self.config.CMAKE, '-G', \
                    'Ninja' if self.__is_ninja_build() else 'Unix Makefiles', \
-                   self.remote.source_dir, \
+                   str(self.remote.source_dir), \
                    '-DCMAKE_C_COMPILER=' + self.config.CC, \
                    '-DCMAKE_CXX_COMPILER=' + self.config.CXX, \
                    '-DCMAKE_MAKE_PROGRAM=' + self.config.NINJA if self.__is_ninja_build() else self.config.MAKE
@@ -180,13 +178,11 @@ class CMakeCommand(Command):
         return ' '.join(command)
 
     def __install(self):
-        command = self.__build()
-        command.append('--target')
-        command.append('install')
+        command = [self.__build(), '--target', 'install']
         return ' '.join(command)
 
     def __build_command(self):
-        return '{gen} && {build} && {install}'.format(gen=self.__generate(), build=self.__build(), install=self.__generate())
+        return '{gen} && {build} && {install}'.format(gen=self.__generate(), build=self.__build(), install=self.__install())
 
 
 class ConanCommand(Command):
@@ -200,59 +196,48 @@ class ConanCommand(Command):
     def is_version_check(self):
         return '-v' in self.argv or '--version' in self.argv
 
-
     def run(self):
         self.argv[0] = self.config.CONAN
-
+        super(ConanCommand, self).run()
 
     def check(self):
-        self.conanfile_py = os.path.join(self.source_dir, 'conanfile.py')
-        self.conanfile_txt = os.path.join(self.source_dir, 'conanfile.txt')
-        if not (os.path.exists(self.local.conanfile_py) \
-            or os.path.exists(self.local.conanfile_txt)):
-            raise RuntimeError("conanfile.py or conanfile.txt does not exists in source directory")
+        self.conanfile_py = Path(self.local.source_dir) / CONAN_PY
+        self.conanfile_txt = Path(self.local.source_dir) / CONAN_TXT
+        assert self.conanfile_py.exists() or self.conanfile_txt.exists(), "conanfile.py or conanfile.txt does not exists in source directory"
 
-
-    def get_conan_home(self):
+    def get_home(self):
         try:
-            return os.environ['CONAN_USER_HOME']
+            return Path(os.environ['CONAN_USER_HOME']).resolve()
         except:
-            return os.environ['HOME']
+            return Path.home()
 
     def make_configurations(self):
-        src_dir = self.get_conan_source_dir()
+        self.local = BuildEnv(self.get_conan_source_dir(), Path(os.getcwd()))
+        self.local.conan_dir = self.get_home() / CONAN_HOME
 
-        self.local = BuildEnv(os.path.abspath(src_dir), os.getcwd())
-        self.local.conan_dir = os.path.join(self.get_home(), '.conan')
-
-
-        self.remote = BuildEnv(self.config.REMOTE_DIR + self.local.source_dir,
-                               self.config.REMOTE_DIR + self.local.build_dir)
-        self.remote.conan_dir = os.path.join(self.get_remote_home(), '.conan')
-        print("remote: " + self.remote.source_dir + "   " + self.remote.build_dir)
-
+        remote = Path(self.config.REMOTE_DIR)
+        self.remote = BuildEnv(remote / self.local.source_dir, remote / self.local.build_dir)
+        self.remote.conan_dir = self.get_remote_home() / CONAN_HOME
+        print("remote: " + str(self.remote.source_dir) + "   " + str(self.remote.build_dir))
 
     def get_remote_home(self):
         conan_home = self.server.getenv('CONAN_USER_HOME').decode("utf-8")
         if not conan_home or conan_home is None:
             return self.server.getenv('HOME').decode("utf-8")
-        return conan_home
+        return Path(conan_home).resolve()
 
+    def __arg_generator(self):
+        i=2
+        while i < len(self.argv):
+            if self.argv[i].startswith('-'):
+                i += ('=' not in arg) + 1
+                continue
+            yield self.argv[i]
+            i += 1
 
     def get_source_dir(self):
-        skip_next=False
-        for arg in self.argv[2:]:
-            if skip_next:
-                skip_next=False
-                continue
-
-            if arg.startswith('-'):
-                if '=' not in arg:
-                    skip_next=True
-                continue
-
-            src_dir = os.path.abspath(arg)
-
-            if os.path.exists(src_dir):
+        for arg in self.__arg_generator():
+            src_dir = Path(arg).resolve()
+            if src_dir.exists():
                 return src_dir
         return None
