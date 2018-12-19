382e178c8419e88f5d789199d98c84f666308e64
Максимов Алексей <maxalex301@gmail.com>
2018-12-19 21:37:32 +0800

    Fix Dockerfile and install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
-mkdir -p bin
+function add_binary {
+    ln -sf ../src/cmd.py $1
+    chmod 555 $1
+}
+
 PYTHON3_PATH=$(which python3)
 sed -e s#/usr/bin/python3#${PYTHON3_PATH}#g src/cmd.py.tmpl > src/cmd.py
-ln -sf src/cmd.py bin/conan
-ln -sf src/cmd.py bin/cmake
-ln -sf src/cmd.py bin/make
-chmod 555 bin/conan
-chmod 555 bin/cmake
-chmod 555 bin/make
+
+mkdir -p bin
+cd bin
+add_binary conan
+add_binary cmake
+cd -
 export PATH=$(pwd)/bin:$PATH
