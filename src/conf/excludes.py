d25fd98cd6b75a67a67521ba6c860d12b0e44494
a.maksimov <a.maksimov@ispsystem.com>
2018-12-19 09:23:55 +0800

    Добавил excludes.py

---
+++ src/conf/excludes.py
@@ -0,0 +1,15 @@
+#Exclude directories for upload
+EXCLUDES=['*.pyc',
+          '.git',
+          '.gitignore',
+          '.idea',
+          '.pytest_cache',
+          '.ssh',
+          '__pycache__',
+          '_build',
+          'cmake-build-debug',
+          'cmake-build-release',
+          'cmake-build-ninja',
+          'msw',
+          'venv'
+          ]
